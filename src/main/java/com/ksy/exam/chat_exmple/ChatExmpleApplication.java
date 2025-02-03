bdf125e8046ace4549fff81327f7ba0324226fb3
kimsiyeonA <kakasska1212@gmail.com>
2025-02-03 14:37:32 +0900

    first commit

---
+++ src/main/java/com/ksy/exam/chat_exmple/ChatExmpleApplication.java
@@ -0,0 +1,13 @@
+package com.ksy.exam.chat_exmple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatExmpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatExmpleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ksy/exam/chat_exmple/ChatExmpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksy.exam.chat_exmple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatExmpleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
